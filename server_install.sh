13294cbe4d816f8421994af80aa9baf679ef25ee
R.J. Kaler <31756802+RJKaler@users.noreply.github.com>
2025-05-26 15:59:52 -0400

    Create server_install.sh

---
+++ server_install.sh
@@ -0,0 +1,148 @@
+#!/bin/bash -e
+
+#This should suffice for a single deployment Jitsi Meet server - but I would not go over two video bridges with this. 
+
+update() { sudo apt-get update; }
+error() { echo "Error. Abort!" && exit 1; }
+
+
+if [[ ! -d ~/logfiles ]]; then
+     mkdir -vp ~/logfiles || error
+     echo "finished creating directory for log file"
+fi
+
+
+log_file="$HOME/logfiles/server_install.log"
+
+
+#GUIDE: https://jitsi.github.io/handbook/docs/devops-guide/devops-guide-quickstart/
+
+#Required packages and repository updates (GUIDE)
+
+if ! command -v curl; then
+	sudo apt-get install curl -y || { echo "error!" && exit 1; }
+fi
+
+{
+
+require_proc() {
+    echo "refreshing package list"
+    #shellcheck disable=SC2015
+    update &&
+        # Ensure support for apt repositories served via HTTPS
+            sudo apt-get install apt-transport-https -y || { error; } &&
+                yes y | sudo apt-add-repository universe || error
+            }
+
+            echo "installing basic packages for server..."
+
+            if require_proc; then
+                echo "successfully installed packages and updated package list"
+            else
+                error
+            fi
+
+#SET UP DNS HERE
+#=================================
+
+#read -rep "Optional: Create a DNS entry for easy server access. Ex: meet.domain.org  (y|n): " ans
+
+#changehost() { sudo hostnamectl set-hostname "$@"; }
+#
+#if [[ "$ans" =~ ^(y|Y|yes|Yes)$ ]]; then
+#    while :
+#    do
+#        read -rep "OK - enter a hostname for your server: " servername
+#        if [[ -z "$servername" ]]; then
+#            echo "Error - no value was entered."
+#        else
+#            echo "Changing host name to $servername"
+#            if changehost "$servername"; then
+#                echo "Success!"
+#                break
+#            else
+#                error
+#            fi
+#        fi
+#    done
+#else
+#    echo "OK - skipping..."
+#fi
+#
+
+#Add the Prosody package repository
+
+#function to install Prosody dependencies
+prosody_proc() {
+    sudo curl -sL https://prosody.im/files/prosody-debian-packages.key -o  /etc/apt/keyrings/prosody-debian-packages.key &&
+        echo "deb [signed-by=/etc/apt/keyrings/prosody-debian-packages.key] http://packages.prosody.im/debian $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/prosody-debian-packages.list &&
+        sudo apt-get install lua5.2 -y && echo "installed lua5.2"
+    }
+
+
+    echo "Installing prosody dependencies..."
+    if prosody_proc; then
+        echo "Successfully added prosody sources"
+        echo "updating all packages..."
+        update || error
+    else
+        error
+    fi
+
+#Add Jitsi package
+
+
+jitsi_proc() {
+    curl -sL https://download.jitsi.org/jitsi-key.gpg.key | sudo sh -c 'gpg --dearmor > /usr/share/keyrings/jitsi-keyring.gpg'
+echo "deb [signed-by=/usr/share/keyrings/jitsi-keyring.gpg] https://download.jitsi.org stable/" | sudo tee /etc/apt/sources.list.d/jitsi-stable.list
+}
+
+    echo "Installing jitsi dependencies..."
+
+
+if jitsi_proc || error; then
+    echo "Successfully added prosody sources"
+    echo "updating all packages..."
+    update || error
+else
+    error
+fi
+
+#configure ufw
+
+sudo ufw enable || error
+
+#shellcheck disable=SC2015
+
+ufw_proc() {
+sudo ufw allow 80/tcp || { error; }
+sudo ufw allow 443/tcp || { error; }
+sudo ufw allow 10000/udp || { error; }
+sudo ufw allow 22/tcp || { error; }
+sudo ufw allow 3478/udp || { error; }
+sudo ufw allow 5349/tcp || { error; }
+}
+
+
+if ufw_proc; then
+    echo "Successfully added prosody sources"
+    echo "updating all packages..."
+    update || error
+fi
+
+echo "Successfully installed dependencies!"
+
+
+echo "Finish your installation by typing: sudo apt install jitsi-meet -y"
+
+
+#if sudo apt install jitsi-meet -y; then
+#    echo "Successfully added prosody sources"
+#    echo "updating all packages..."
+#    update || { error; }
+#else
+#    error
+#fi
+
+
+} | tee -a "$log_file"
