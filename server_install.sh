613e10e24138a11dc8e868684fb708b8faf1c93f
R.J. Kaler <31756802+RJKaler@users.noreply.github.com>
2025-05-28 01:29:24 -0400

    Update server_install.sh

--- server_install.sh
+++ server_install.sh
@@ -1,5 +1,28 @@
 #!/bin/bash -e
 
+# Jitsi Meet Deployment Script
+# Copyright (C) 2025 Richard Kaler <richardkaler@gmail.com>
+#
+# This script is licensed under the GNU General Public License v3.0 or later.
+#
+# Runner Technologies is granted a perpetual, royalty-free license to use,
+# modify, and distribute this script under the terms of the GNU GPL v3,
+# as the original client for whom this was developed.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 #This should suffice for a single deployment Jitsi Meet server - but I would not go over two video bridges with this. 
 
 update() { sudo apt-get update; }
